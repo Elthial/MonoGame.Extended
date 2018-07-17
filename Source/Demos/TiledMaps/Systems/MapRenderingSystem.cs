3f325f6a2a4a7f2e00698db6cbdf636ce566d189
Dylan Wilson <dylan@craftworkgames.com>
2018-07-17 21:16:25 +1000

    cleaned up some input and camera stuff

--- Source/Demos/TiledMaps/Systems/MapRenderingSystem.cs
+++ Source/Demos/TiledMaps/Systems/MapRenderingSystem.cs
@@ -1,24 +1,36 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using MonoGame.Extended;
 using MonoGame.Extended.Entities.Systems;
+using MonoGame.Extended.Tiled;
+using MonoGame.Extended.Tiled.Renderers;
 
 namespace TiledMaps.Systems
 {
     public class MapRenderingSystem : DrawSystem
     {
-        private readonly SpriteBatch _spriteBatch;
+        private readonly GraphicsDevice _graphicsDevice;
+        private readonly Camera _camera;
+        private readonly TiledMapRenderer _mapRenderer;
 
-        public MapRenderingSystem(GraphicsDevice graphicsDevice)
+        public MapRenderingSystem(ContentManager contentManager, GraphicsDevice graphicsDevice, Camera camera)
         {
-            _spriteBatch = new SpriteBatch(graphicsDevice);
+            _graphicsDevice = graphicsDevice;
+            _camera = camera;
+
+            var map = contentManager.Load<TiledMap>("test-map-1");
+            _mapRenderer = new TiledMapRenderer(graphicsDevice, map);
         }
 
         public override void Draw(GameTime gameTime)
         {
-            _spriteBatch.Begin();
-            _spriteBatch.FillRectangle(100, 100, 100, 100, Color.AliceBlue);
-            _spriteBatch.End();
+            _graphicsDevice.Clear(Color.CornflowerBlue);
+            _graphicsDevice.BlendState = BlendState.NonPremultiplied;
+            _graphicsDevice.SamplerStates[0] = SamplerState.PointClamp;
+            _graphicsDevice.RasterizerState = RasterizerState.CullCounterClockwise;
+
+            _mapRenderer.Draw(layerIndex: 0, viewMatrix: _camera.GetViewMatrix());
         }
     }
 
---
+++ Source/MonoGame.Extended.Input/KeyboardService.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace MonoGame.Extended.Input
+{
+    public class KeyboardService
+    {
+        public KeyboardState CurrentKeyboardState { get; private set; }
+        public KeyboardState PreviousKeyboardState { get; private set; }
+
+        public bool CapsLock => CurrentKeyboardState.CapsLock;
+        public bool NumLock => CurrentKeyboardState.NumLock;
+        public bool IsShiftDown() => CurrentKeyboardState.IsKeyDown(Keys.LeftShift) || CurrentKeyboardState.IsKeyDown(Keys.RightShift);
+        public bool IsControlDown() => CurrentKeyboardState.IsKeyDown(Keys.LeftControl) || CurrentKeyboardState.IsKeyDown(Keys.RightControl);
+        public bool IsAltDown() => CurrentKeyboardState.IsKeyDown(Keys.LeftAlt) || CurrentKeyboardState.IsKeyDown(Keys.RightAlt);
+        public bool IsKeyDown(Keys key) => CurrentKeyboardState.IsKeyDown(key);
+        public bool IsKeyUp(Keys key) => CurrentKeyboardState.IsKeyUp(key);
+        public Keys[] GetPressedKeys() => CurrentKeyboardState.GetPressedKeys();
+
+        public bool WasKeyJustDown(Keys key) => PreviousKeyboardState.IsKeyDown(key) && CurrentKeyboardState.IsKeyUp(key);
+        public bool WasKeyJustUp(Keys key) => PreviousKeyboardState.IsKeyUp(key) && CurrentKeyboardState.IsKeyDown(key);
+        public bool WasAnyKeyJustDown() => PreviousKeyboardState.GetPressedKeys().Any();
+
+        public void Update(GameTime gameTime)
+        {
+            PreviousKeyboardState = CurrentKeyboardState;
+            CurrentKeyboardState = Keyboard.GetState();
+        }
+    }
+}
